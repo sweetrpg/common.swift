ecf9d22bae32d7f7f348af8a8a1887728d362e3e
Paul Schifferer <paul@schifferers.net>
2024-11-13 12:11:45 -0800

    Fix Swift names

--- Tests/CommonTests/CommonTests.swift
+++ Tests/CommonTests/CommonTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import CatalogObjects
+@testable import Common
 
-final class CatalogObjectsTests: XCTestCase {
+final class CommonTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
